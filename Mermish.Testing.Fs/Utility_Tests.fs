1593c5fff3b6d0d33e6ff6c1bdea8a3a43d48c17
David Andrews <dave@sanitychallenged.com>
2022-10-24 13:30:04 -0400

    UMap has everything I need so far

--- Mermish.Testing.Fs/Utility_Tests.fs
+++ Mermish.Testing.Fs/Utility_Tests.fs
@@ -23,6 +23,23 @@ module UMap =
         result |> should equal original
 
 
+    let ``Order is preserved when converting from Seq``() =
+        let original = seq { (3, 3) ; (1, 1) ; (2, 2) }
+        let result = original |> UMap.ofSeq |> UMap.toSeq
+
+        result |> should haveLength (Seq.length original)
+        Seq.zip original result
+        |> Seq.iter (fun (original', result') -> result' |> should equal original')
+
+
+    [<Fact>]
+    let ``Order is preserved when converting from List``() =
+        let original = [ (2, 2) ; (1, 1) ; (3, 3) ]
+        let result = original |> UMap.ofList |> UMap.toList
+
+        result |> should equal original
+
+
     [<Fact>]
     let ``Is Enumerable``() =
         let mutable count = 0
@@ -68,5 +85,69 @@ module UMap =
         result["1"] |> should equal 1
         result["2"] |> should equal 4
         result["3"] |> should equal 3
+
+
+    [<Fact>]
+    let ``Can add``() =
+        let result = 
+            [ "first", true ; "second", false ; "third", true ]
+            |> UMap.ofSeq
+            |> UMap.add "added" true
+
+        result |> should haveLength 4
+        result["added"] |> should equal true
         
+
+    [<Fact>]
+    let ``Order is preserved when using add``() =
+        let result = 
+            [ "first", true ; "second", false ; "third", true ]
+            |> UMap.ofSeq
+            |> UMap.add "added" true
+
+        result |> UMap.toSeq |> Seq.last |> should equal ("added", true)
         
+
+    [<Fact>]
+    let ``Can overwrite with add``() =
+        [ (true, 1) ; (false, 2)]
+        |> UMap.ofList
+        |> UMap.add true 3
+        |> UMap.toList
+        |> should equal [ (false, 2) ; (true, 3) ]
+
+
+    [<Fact>]
+    let ``Can addAll``() =
+        [ (1, 1) ; (2, 2) ]
+        |> UMap.ofSeq
+        |> UMap.addAll [ (3, 3) ; (4, 4) ]
+        |> UMap.toList
+        |> should equal [ (1, 1) ; (2, 2) ; (3, 3) ; (4, 4) ]
+
+
+    [<Fact>]
+    let ``Order is preserved when using addAll``() =
+        [ (4, 1) ; (1, 1) ]
+        |> UMap.ofSeq
+        |> UMap.addAll [ (3, 1) ; (2, 1) ]
+        |> UMap.toList
+        |> should equal [ (4, 1) ; (1, 1) ; (3, 1) ; (2, 1) ]
+
+
+    [<Fact>]
+    let ``Can overwrite with addAll``() =
+        [ (1, 1) ; (2, 2) ]
+        |> UMap.ofSeq
+        |> UMap.addAll [ (3, 3) ; (2, 4) ; (3, 5) ; (4, 6) ]
+        |> UMap.toList
+        |> should equal [ (1, 1) ; (2, 4) ; (3, 5) ; (4, 6) ]
+
+
+    [<Fact>]
+    let ``Can remove``() =
+        [ "a", true ; "b", false ; "c", true ]
+        |> UMap.ofSeq
+        |> UMap.remove "b"
+        |> UMap.toList
+        |> should equal [ "a", true ; "c", true ]
