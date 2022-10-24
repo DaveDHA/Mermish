d401165c0f6b4a5b0a552efe160b53bc1eb3f1ce
David Andrews <dave@sanitychallenged.com>
2022-10-23 20:09:34 -0400

    Starting to add GanttChart

---
+++ Mermish/GanttChart.fs
@@ -0,0 +1,22 @@
+﻿namespace Mermish
+
+open System
+
+
+[<StructuredFormatDisplay("{MermaidSyntax}")>]
+type GanttChart = {
+    Title : string
+    DateFormat : string
+}
+    with        
+        member this.MermaidSyntax =
+            seq {
+                yield "gantt"
+                if not (String.IsNullOrEmpty this.Title) then yield $"    title {this.Title}"
+                if not (String.IsNullOrEmpty this.DateFormat) then yield $"    dateFormat {this.DateFormat}"
+            }
+            |> String.concat "\n"
+
+        override this.ToString() = this.MermaidSyntax
+
+        interface IMermaidChart with member this.MermaidSyntax = this.MermaidSyntax
