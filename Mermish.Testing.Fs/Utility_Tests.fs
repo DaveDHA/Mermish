71c0b2842c89f727faff83993fa7a1e98147ccee
David Andrews <dave@sanitychallenged.com>
2022-10-24 13:09:52 -0400

    Working on UMap

---
+++ Mermish.Testing.Fs/Utility_Tests.fs
@@ -0,0 +1,72 @@
+﻿namespace Mermish.Testing.Fs.Utility
+
+open Xunit
+open FsUnit.Xunit
+open Mermish.Utility
+
+module UMap =
+    [<Fact>]
+    let ``Can create from and convert to Seq``() =
+        let original = seq { (1, 1) ; (2, 2) ; (3, 3) }
+        let result = original |> UMap.ofSeq |> UMap.toSeq
+
+        result |> should haveLength (Seq.length original)
+        Seq.zip original result
+        |> Seq.iter (fun (original', result') -> result' |> should equal original')
+
+
+    [<Fact>]
+    let ``Can create from and convert to List``() =
+        let original = [ (1, 1) ; (2, 2) ; (3, 3) ]
+        let result = original |> UMap.ofList |> UMap.toList
+
+        result |> should equal original
+
+
+    [<Fact>]
+    let ``Is Enumerable``() =
+        let mutable count = 0
+
+        let map = 
+            [ (1, 1) ; (2, 2) ; (3, 3) ]
+            |> UMap.ofSeq
+
+        for (k,v) in map do
+            count <- count + 1
+            k |> should equal v
+
+        count |> should equal 3
+
+
+    [<Fact>]
+    let ``Is Indexable by Key``() =
+        let result = UMap.ofSeq [ (1, 1) ; (2, 2) ; (3, 3) ]
+        result[1] |> should equal 1
+        result[2] |> should equal 2
+        result[3] |> should equal 3
+        (fun () -> result[4] |> ignore) |> should throw typeof<System.ArgumentException>
+
+
+
+    [<Fact>]
+    let ``Duplicate keys overwrite when converting from Seq``() =
+        let original = seq { (1, "1") ; (2, "2") ; (3, "3") ; (2, "4") }
+        let result = original |> UMap.ofSeq
+
+        result |> should haveLength 3
+        result[1] |> should equal "1"
+        result[2] |> should equal "4"
+        result[3] |> should equal "3"
+
+
+    [<Fact>]
+    let ``Duplicate keys overwrite when converting from List``() =
+        let original = [ ("2", 2) ; ("1", 1) ; ("3", 3) ; ("2", 4) ]
+        let result = original |> UMap.ofList
+
+        result |> should haveLength 3
+        result["1"] |> should equal 1
+        result["2"] |> should equal 4
+        result["3"] |> should equal 3
+        
+        
--- Mermish/Utility.fs
+++ Mermish/Utility.fs
@@ -9,30 +9,46 @@ module Tuple =
 
     let mapSnd mapper (a, b) = (a, mapper b)
 
-
-module Map =
-    let addAll items map =
-        items
-        |> Seq.fold (fun m (a,b) -> Map.add a b m) map
     
 
 // UMap is a Map that preserves the order of items added
-type UMap<'k,'v> = private UMap of ('k * 'v) list
+type UMap<'k,'v when 'k : equality> = private UMap of ('k * 'v) list    
     with
+        member private this.InnerList = let (UMap lst) = this in lst
+
+
+        member this.Item with get key = 
+            this.InnerList
+            |> List.tryFind (fun (k,_) -> k = key)
+            |> function
+                | Some (_,v) -> v
+                | None -> invalidArg (nameof key) "The index was outside the range of elements in the UMap."
+
+
+        member this.Length = this.InnerList.Length
+
+
         interface IEnumerable<'k * 'v> with
             member this.GetEnumerator() = let (UMap lst) = this in (lst :> IEnumerable<'k * 'v>).GetEnumerator()
+
 
         interface IEnumerable with
             member this.GetEnumerator() = (this :> IEnumerable<'k * 'v>).GetEnumerator()
 
 
 module UMap =
-    let empty<'k, 'v> = List.empty<'k * 'v> |> UMap
-
-    let ofSeq items = items |> List.ofSeq |> UMap
-    let ofList items = UMap items
+    let empty<'k, 'v when 'k : equality> = List.empty<'k * 'v> |> UMap
+    
+    // The double reverse is because we want the last item for a given key.
+    // Not the greatest perf, but fine for our purpose in this library.
+    let ofSeq items = items |> Seq.rev |> Seq.distinctBy fst |> Seq.rev |> List.ofSeq |> UMap
+    
+    let ofList items = items |> List.toSeq |> ofSeq
+    
     let toSeq (UMap items) = items |> List.toSeq
+    
     let toList (UMap items) = items
+
 
     let add key value (UMap items) =
         match items |> List.tryFindIndex (fun (k,_) -> k = key) with
